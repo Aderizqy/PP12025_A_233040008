c5c981e16286c41e40c05fdc2b77517bd84ad780
Aderizqy <aderizqy99@gmail.com>
2025-05-06 20:39:30 +0700

    latihan & tugas pertemuan 7

---
+++ pertemuan_7/latihan_1/StrukturList.java
@@ -0,0 +1,19 @@
+package pertemuan_7.latihan_1;
+
+import pertemuan_7.Node;
+
+public class StrukturList {
+    Node HEAD;
+
+    public boolean find (int x) {
+        Node curNode = HEAD;
+        boolean ketemu = false;
+
+        while (curNode != null && !ketemu) {
+            if (curNode.getData()==x)
+                ketemu = true;
+            else curNode = curNode.getNext();
+        }
+        return ketemu;
+    }
+}
